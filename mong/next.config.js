ee4426d194268ba317a88e031b42aed9e312a1bc
perfect1337 <168024739+perfect1337@users.noreply.github.com>
2025-05-16 23:02:39 +0400

    Delete mong directory

--- mong/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com'], // Add any image domains you plan to use
-  },
-}
-
-module.exports = nextConfig 
